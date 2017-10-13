45eff39b76355f30ea4a4717351e5696e53109f6
ancyk <ankawlodarek@gmail.com>
2017-10-13 19:16:28 +0200

    Poprawa menu, wreszcie działa, jak należy :)

--- js/script.js
+++ js/script.js
@@ -4,52 +4,54 @@
   var nr;
   
   
-  
-  
+  // ukrycie lub pokazanie menu w zależności
+  // od szerokości okna przeglądarki
   function changeMenuDevice() {
     if ($window.outerWidth(true) < 900) {
-      $topMenu.slideUp(); 
+      $topMenu.hide(); 
       $topMenu.addClass('mobile');
-      $('nav').on('click', function() {
-        $topMenu.slideUp();
-      });
     }
     else {
-      $topMenu.slideDown();
+      $topMenu.show();
       $topMenu.removeClass('mobile');
-      $('nav').off('click', function() {
-        $topMenu.slideUp();
-      });
     } 
   }
   
   changeMenuDevice();
   
+  // ukrycie tekstu
+  $('.hidden-text').hide();
+  
+  // reakcja na zmianę rozmiaru okna
   $window.on('resize', function() {
     changeMenuDevice();
   });
   
+  // rozwijanie/zwijanie menu mobilnego
+  $('#menu-bar').on('click', function() {
+    $topMenu.slideToggle();
+  });
   
+  // zwinięcie menu mobilnego po kliknięciu
+  $('nav>a').on('click', function() {
+    if ($topMenu.hasClass('mobile') && ($window.outerWidth(true) < 900)) $topMenu.slideUp();
+  });
   
-  
-  $('.hidden-text').slideUp();
-  
+  // przejście niżej po kliknięciu strzałki
   $('#arrow').on('click', function() {
     $('html, body').animate({
       scrollTop: $('#ofirmie>h1').offset().top - $('div.navbar').outerHeight(true)
     }, 1000, 'swing');
   });
   
-  $('#menu-bar').on('click', function() {
-    $topMenu.slideToggle();
-  });
-  
+  // rozwijanie dalszej części tekstu
   $('button').on('click', function(e) {
     e.preventDefault();
     nr = (this.id).substring(4,5);
     $('.hidden' + nr).slideToggle();
   });
   
+  // przejście do góry po kliknięciu
   $('#hide1').on('click', function(e) {
     e.preventDefault();
     $('.hidden1').slideUp();
