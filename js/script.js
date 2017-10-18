9b13ffca69da8e263ac12782970d5bb9ad31f185
ancyk <ankawlodarek@gmail.com>
2017-10-19 00:31:58 +0200

    Zachowanie proporcji filmików przy zmianie rozmiaru okna

--- js/script.js
+++ js/script.js
@@ -70,6 +70,8 @@
   // rozwijanie/zwijanie menu mobilnego
   $('#menu-bar').on('click', function() {
     $topMenu.slideToggle();
+    $('#menu-bar i').toggleClass('fa-bars');
+    $('#menu-bar i').toggleClass('fa-times');
   });
   
   // zwinięcie menu mobilnego po kliknięciu
@@ -104,6 +106,38 @@
   $('.product-hover').on('click', function() {
     nrProduct = parseInt((this.id).slice(-1));
     hideProducts(nrProduct);
+    $('html, body').animate({
+      scrollTop: $('#product-content').offset().top -100
+    }, 500, 'swing');
   });
+  
+  // zachowanie proporcji filmiku
+  var iframes = document.getElementsByTagName( 'iframe' );
+
+  for ( var i = 0; i < iframes.length; i++ ) {
+    var iframe = iframes[i],
+    players = /www.youtube.com/;
+    if ( iframe.src.search( players ) > 0 ) {
+      var videoRatio = ( iframe.height / iframe.width ) * 100;
+      
+      iframe.style.position = 'absolute';
+      iframe.style.top      = '0';
+      iframe.style.left     = '0';
+      iframe.width          = '100%';
+      iframe.height         = '100%';
+
+      var wrap              = document.createElement( 'div' );
+      wrap.className        = 'fluid-vids';
+      wrap.style.width      = '100%';
+      wrap.style.position   = 'relative';
+      wrap.style.paddingTop = videoRatio + '%';
+      
+      var iframeParent      = iframe.parentNode;
+      iframeParent.insertBefore( wrap, iframe );
+      wrap.appendChild( iframe );
+
+    }
+
+  }
   
 })();
