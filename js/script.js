06b230a6eac302a32026429b27baa44584b0c718
ancyk <ankawlodarek@gmail.com>
2017-10-19 03:06:25 +0200

    Poprawki w wyglądzie

--- js/script.js
+++ js/script.js
@@ -112,32 +112,29 @@
   });
   
   // zachowanie proporcji filmiku
-  var iframes = document.getElementsByTagName( 'iframe' );
-
-  for ( var i = 0; i < iframes.length; i++ ) {
+  var iframes = document.getElementsByTagName('iframe');
+  for (var i = 0; i < iframes.length; i++) {
     var iframe = iframes[i],
     players = /www.youtube.com/;
-    if ( iframe.src.search( players ) > 0 ) {
-      var videoRatio = ( iframe.height / iframe.width ) * 100;
+    if (iframe.src.search(players) > 0) {
+      var videoRatio = (iframe.height/iframe.width) * 100;
       
       iframe.style.position = 'absolute';
-      iframe.style.top      = '0';
-      iframe.style.left     = '0';
-      iframe.width          = '100%';
-      iframe.height         = '100%';
+      iframe.style.top = '0';
+      iframe.style.left = '0';
+      iframe.width = '100%';
+      iframe.height = '100%';
 
-      var wrap              = document.createElement( 'div' );
-      wrap.className        = 'fluid-vids';
-      wrap.style.width      = '100%';
-      wrap.style.position   = 'relative';
+      var wrap = document.createElement('div');
+      wrap.className = 'fluid-vids';
+      wrap.style.width = '100%';
+      wrap.style.position = 'relative';
       wrap.style.paddingTop = videoRatio + '%';
       
-      var iframeParent      = iframe.parentNode;
-      iframeParent.insertBefore( wrap, iframe );
-      wrap.appendChild( iframe );
-
+      var iframeParent = iframe.parentNode;
+      iframeParent.insertBefore(wrap, iframe);
+      wrap.appendChild(iframe);
     }
-
   }
   
 })();
