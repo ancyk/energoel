203e54c93516e2d5bcbd323f260fb16c3223f462
ancyk <ankawlodarek@gmail.com>
2017-10-21 21:49:14 +0200

    Poprawka przycisku do ukrywania ofert, funkcjonalność przycisku zwijającego opis produktu, zmiana stylu menu przy scrollowaniu, efekt parallax, zmiana aktywnego elementu menu, przeniesienie obrazka zębatek pod tekst na urządzeniach mobilnych, powrót na górę strony po wciśnięciu loga, płynne przewijanie strony po kliknięciu elementu menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 (function(){
   var $window = $(window);
   var $topMenu = $('#top-menu');
+  var $htmlBody = $('html, body');
   var $showMobile = $('#show-mobile');
   var $hideMobile = $('#hide-mobile');
   var $hiddenTextMobile = $('.hidden-text-mobile');
-  var nrOffer, nrProduct;
+  var $productContent = $('#product-content');
+  var $images = $('.parallax');
+  var nrOffer, nrProduct, menuSrc, $winScroll, $imgOffset, $imgHeight, zebatki, zebatkiHr;
   
   
   // ukrycie tekstu oraz przycisków
   $('.hidden-text').hide(); 
   $showMobile.hide();
   $hideMobile.hide();
+  $productContent.hide();
+  
   
   // ukrycie lub pokazanie menu w zależności
   // od szerokości okna przeglądarki
@@ -25,46 +30,115 @@
     } 
   }
   
+  // ukrycie lub pokazanie ofert
   function offerShow() {
-    if ($window.outerWidth(true) < 900) {
-      $showMobile.show();
-      $hiddenTextMobile.hide();
-      $showMobile.on('click', function(e){
-        e.preventDefault();
-        $hiddenTextMobile.slideDown();
-        $showMobile.hide();
-        $hideMobile.show();
-      });
-      $hideMobile.on('click', function(e) {
-        e.preventDefault();
-        $hiddenTextMobile.slideUp();
-        $showMobile.show();
-        $('html, body').animate({
-          scrollTop: $showMobile.offset().top - 100
-        });
-      });
-    } else {
+    $showMobile.show();
+    $hiddenTextMobile.hide();
+    $showMobile.on('click', function(e){
+      e.preventDefault();
+      $hiddenTextMobile.slideDown();
       $showMobile.hide();
-      $hideMobile.hide();
-      $hiddenTextMobile.show();
-    }
+      $hideMobile.show();
+      $hiddenTextMobile.addClass('mobile');
+    });
+    $hideMobile.on('click', function(e) {
+      e.preventDefault();
+      $hiddenTextMobile.slideUp();
+      $showMobile.show();
+      $hiddenTextMobile.removeClass('mobile');
+      $htmlBody.animate({
+        scrollTop: $showMobile.offset().top - 80
+      });
+    });
   }
   
+  // wyświetlenie poszczególnego produktu
   function hideProducts(nr) {
     for (var i = 1; i <= $('#product-content').children().length; i++) {
       if (i != nr) $('#product' + i).hide();
-      else $('#product' + i).slideDown();
+      else {
+        $('#product' + i).slideDown();
+        $productContent.show();
+      }
+    }
+  }
+  
+  // zmiana stylu menu
+  function changeMenuStyle() {
+    if ($winScroll >= 300)
+      $('.navbar').addClass('scroll-menu');
+    else
+      $('.navbar').removeClass('scroll-menu');
+  }
+  
+  // efekt parallax
+  function parallaxEffect() {
+    $images.each(function() {
+      $imgOffset = $(this).offset().top;
+      $imgHeight = $(this).height();
+
+      if ($window.width() < 780) {
+        $(this).css('backgroundSize', '250%');
+      } else {
+        $(this).css('backgroundSize', 'cover');
+      }
+            
+      $(this).css({
+        backgroundPosition: '50%' + ' ' + (Math.round(($imgOffset - $winScroll)*3/8)-120) + 'px'
+      });
+    });
+  }
+  
+  // aktywny element menu
+  function activeElement() {
+    if ($window.width() > 900) {
+      if (($winScroll > ($('.bg-header').offset().top + 300)) && ($winScroll < ($('.bg2').offset().top - 300))) { $('a[href="#ofirmie"]').addClass('current'); } else { $('a[href="#ofirmie"]').removeClass('current'); }    
+
+      if (($winScroll >= ($('.bg2').offset().top - 300)) && ($winScroll < ($('.bg3').offset().top - 300))) { $('a[href="#produkty"]').addClass('current'); } else { $('a[href="#produkty"]').removeClass('current'); }
+
+      if (($winScroll >= ($('.bg3').offset().top - 300)) && ($winScroll < ($('.bg4').offset().top - 300))) { $('a[href="#oferta"]').addClass('current'); } else { $('a[href="#oferta"]').removeClass('current'); }
+
+      if (($winScroll >= ($('.bg4').offset().top - 300)) && ($winScroll < ($('.bg5').offset().top - 300))) { $('a[href="#technologie"]').addClass('current'); } else { $('a[href="#technologie"]').removeClass('current'); }
+
+      if (($winScroll >= ($('.bg5').offset().top - 300)) && ($winScroll < $('footer').offset().top)) { $('a[href="#kontakt"]').addClass('current'); } else { $('a[href="#kontakt"]').removeClass('current'); }
+    } else {
+      $('.top-menu a').each(function() {
+        $(this).removeClass('current');
+      });
     }
   }
   
   changeMenuDevice();
-  offerShow(); 
+  changeMenuStyle();
+  if ($window.outerWidth(true) < 900) {
+    offerShow(); 
+    zebatki = document.getElementById('zebatki');
+    zebatkiHr = document.querySelector('#zebatki > hr');
+    zebatki.removeChild(zebatkiHr);
+    document.querySelector('#show7 + .hidden7').appendChild(zebatki);
+  }
   hideProducts();
 
   // reakcja na zmianę rozmiaru okna
   $window.on('resize', function() {
     changeMenuDevice();
-    offerShow();
+    activeElement();
+    if (($window.outerWidth(true) < 900) && (!$hiddenTextMobile.hasClass('mobile'))) offerShow();
+    else if ($window.outerWidth(true) >= 900) {
+      $showMobile.hide();
+      $hideMobile.hide();
+      $hiddenTextMobile.show();
+      $hiddenTextMobile.removeClass('mobile');
+    }
+  });
+  
+  // reakcja na scrollowanie
+  $window.on('scroll', function() {
+    $winScroll = $window.scrollTop();
+    
+    changeMenuStyle();
+    parallaxEffect();
+    activeElement();
   });
   
   // rozwijanie/zwijanie menu mobilnego
@@ -76,13 +150,17 @@
   
   // zwinięcie menu mobilnego po kliknięciu
   $('nav>a').on('click', function() {
-    if ($topMenu.hasClass('mobile') && ($window.outerWidth(true) < 900)) $topMenu.slideUp();
+    if ($topMenu.hasClass('mobile') && ($window.outerWidth(true) < 900)) {
+      $topMenu.slideUp();
+      $('#menu-bar i').toggleClass('fa-bars');
+      $('#menu-bar i').toggleClass('fa-times');
+    }
   });
   
   // przejście niżej po kliknięciu strzałki
   $('#arrow').on('click', function() {
-    $('html, body').animate({
-      scrollTop: $('#ofirmie>h1').offset().top - $('div.navbar').outerHeight(true)
+    $htmlBody.animate({
+      scrollTop: $('#ofirmie > h1').offset().top - 80
     }, 1000, 'swing');
   });
   
@@ -93,22 +171,44 @@
     $('.hidden' + nrOffer).slideToggle();
   });
   
-  // przejście do góry po kliknięciu
+  // przejście do góry po kliknięciu opisu
   $('#hide1').on('click', function(e) {
     e.preventDefault();
     $('.hidden1').slideUp();
-    $('html, body').animate({
-      scrollTop: $('#show1').offset().top - 100
+    $htmlBody.animate({
+      scrollTop: $('#show1').offset().top - 80
     }, 500, 'swing');
+  });
+  
+  // przejście do góry strony
+  $('#logo').on('click', function() {
+    $htmlBody.animate({
+      scrollTop: 0
+    }, 1500, 'swing');
   });
   
   // pokazywanie opisu produktu
   $('.product-hover').on('click', function() {
     nrProduct = parseInt((this.id).slice(-1));
     hideProducts(nrProduct);
-    $('html, body').animate({
-      scrollTop: $('#product-content').offset().top -100
+    $htmlBody.animate({
+      scrollTop: $('#product-content').offset().top - 80
     }, 500, 'swing');
+  });
+  
+  // zwinięcie opisu produktu
+  $('.btn-product').on('click', function(e) {
+    e.preventDefault();
+    $productContent.slideUp();
+  });
+  
+  // efekt przejścia w menu
+  $('#top-menu').on('click', 'a', function(e) {
+    e.preventDefault();
+    menuSrc = this.getAttribute('href');
+    $htmlBody.animate({
+      scrollTop: $(menuSrc + ' h1').offset().top - 80
+    }, 1500, 'swing');
   });
   
   // zachowanie proporcji filmiku
